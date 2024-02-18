b5a5440444e664e52269f00db2033bfb16050dfc
Einar Jensen <ebjensen03@gmail.com>
2024-02-18 11:55:36 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class MyFirstProgram{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is OOP!");
+        }
+    }
+}
